09fd71622ef282407a8fad45a2ef1a516b2d4499
nirjalprmr024 <nirjalparmarwork1@gmail.com>
2026-04-05 13:47:02 +0530

    Set DEBUG to False and allow all hosts

--- knowledge_vault/settings.py
+++ knowledge_vault/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4x^89kto-5v4+5-w0a0lye^k2%4t#qegng_*9dvg(he1m3le2_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
